ebd10e862c9675d41c9f933973c0e5654d447537
Mightysam01 <olaleyemightysam@yahoo.com>
2025-06-18 14:31:02 +0100

    update to App.js

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -27,7 +27,7 @@ function App() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://localhost:5000/predict', {
+    const response = await fetch('https://regression-house-price-model.onrender.com/predict', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData),
